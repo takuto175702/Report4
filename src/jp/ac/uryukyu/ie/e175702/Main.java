4e0e523392c396aa6aca8e32a341a9089fb5be4f
takuto175702 <Takuttyo0918>
2017-11-21 08:44:38 +0900

    課題レポート4　ステップ2 try-catch

--- src/jp/ac/uryukyu/ie/e175702/Main.java
+++ src/jp/ac/uryukyu/ie/e175702/Main.java
@@ -3,11 +3,13 @@ package jp.ac.uryukyu.ie.e175702;
 public class Main {
     public static void main(String[] args){
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            double value = Double.parseDouble(str);
+
         }catch(NullPointerException e){
             System.out.println("例外が発生しました!!");
             System.out.println("代入されている文字列：" + e.getMessage());
+
 
 
         }
